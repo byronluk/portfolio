b8aa7af1621869fce524b853a284a18a7ab34b0f
Byron Luk <byronluk@gmail.com>
2018-01-25 21:58:04 -0800

    set up partials and static html

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const app = new express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.set('views', './views/pages');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  const data = {
+    person: {
+      firstName: 'Byron',
+      lastName: 'Luk'
+    },
+    title: 'My Homepage',
+  }
+  res.render('index', data);
+});
+
+app.get('/portfolio', (req, res) => {
+  res.render('portfolio', { title: 'Byron\'s Portfolio' });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About Me' });
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact', { title: 'Get in Touch' });
+});
+
+app.post('/thanks', (req, res) => {
+  res.render('thanks', { contact: req.body, title: 'Thanks ' + req.body.firstName });
+});
+
+app.get('*', (req, res) => {
+  res.send('Page not found 404').status(404);
+})
+
+app.listen(8080, () => {
+  console.log('Server is listening on http://localhost:8080');
+});
